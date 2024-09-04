ff4b736f5f477b1d8fe6387cbf2b8cebb0ca9fda
Davide Giliberti <gilo.davide@hotmail.it>
2024-09-04 15:27:02 +0200

    initial commit

---
+++ src/main/java/davidegiliberti/epicode_3_9_24/Epicode3924Application.java
@@ -0,0 +1,13 @@
+package davidegiliberti.epicode_3_9_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epicode3924Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epicode3924Application.class, args);
+	}
+
+}
